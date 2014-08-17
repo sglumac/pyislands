f5dcfbdfb7433a1f7df80c855ef95250217ef41c
sglumac <slaven.glumac@gmail.com>
2014-08-17 12:39:54 +0200

    little permutation coding

--- pyislands/permutation/generate.py
+++ pyislands/permutation/generate.py
@@ -1,10 +1,12 @@
 from random import shuffle
 
 def random_permutation(num_elements):
-    tmp = range(1, num_elements)
+    ''' generates random permutation, e.g. (0, n-1, 3, ..., 1, n - 1) '''
+    tmp = range(num_elements)
     shuffle(tmp)
 
     return tuple(tmp)
 
 def unit(num_elements):
-    return tuple(range(1, num_elements + 1))
+    ''' generates unit permutation, (0, 1, 2, ..., n - 1)
+    return tuple(range(num_elements))
