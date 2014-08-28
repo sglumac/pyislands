f5fa6e6c69180b03cb49e2edd55f32aa8db30cfb
sglumac <slaven.glumac@gmail.com>
2014-08-28 23:05:57 +0200

    profiling on crossover completed

--- tests/permutation/testcrossover.py
+++ tests/permutation/testcrossover.py
@@ -1,6 +1,7 @@
 from pyislands.permutation.crossover import cyclic_crossover
 from pyislands.permutation.crossover import partially_mapped_crossover
 from pyislands.permutation.crossover import pmx_find
+from pyislands.permutation.crossover import gen_pmx_child
 
 
 def test_cx1():
@@ -27,6 +28,7 @@ def test_pmx():
     parent1_genotype = (0, 1, 2, 3, 4, 5, 6)
     parent2_genotype = (6, 4, 0, 2, 1, 5, 3)
 
+
     children_genotypes = partially_mapped_crossover(parent1_genotype, parent2_genotype)
 
     for genotype in children_genotypes:
@@ -43,3 +45,15 @@ def test_pmx_find():
 
     assert pmx_find(0, swath1_dict) == 4
     assert pmx_find(2, swath2_dict) == 0
+
+
+def test_pmx_child():
+    parent1 = (0, 1, 2, 3, 4, 5, 6)
+    parent2 = (6, 4, 0, 2, 1, 5, 3)
+    break1, break2 = 1, 6
+
+    swath1, swath2 = parent2[break1:break2], parent1[break1:break2]
+
+    child1 = gen_pmx_child(parent1, swath1, swath2, break1, break2, len(parent1))
+
+    assert child1 ==  (3, 4, 0, 2, 1, 5, 6)
