f49859c37667fe0ce7f15f5ce6f928847f450550
sglumac <slaven.glumac@gmail.com>
2014-08-31 17:06:48 +0200

    rsm edited

--- pyislands/permutation/mutation/rsm.py
+++ pyislands/permutation/mutation/rsm.py
@@ -1,22 +1,29 @@
 import random
 import functools as fcn
+from itertools import chain, islice
 
 
 def get_reversed_sequence_mutation(mutation_probability):
     '''
     Reversed Sequence Mutation - ignored with
     probability (1 - mutation_probability)
+
+    In tsp, equivalent to exchange of two edges.
     '''
 
     return fcn.partial(__reversed_sequence_mutation, mutation_probability)
 
 
 def __reversed_sequence_mutation(mutation_probability, genotype):
-    new_genotype = list(genotype)
 
-    if random.random() < mutation_probability:
-        num_genes = len(genotype)
-        break1, break2 = sorted(random.sample(range(num_genes + 1), 2))
-        new_genotype[break1:break2] = reversed(genotype[break1:break2])
+    if not random.random() < mutation_probability:
+        return genotype
 
-    return tuple(new_genotype)
+    num_genes = len(genotype)
+    break1, break2 = sorted(random.sample(range(num_genes + 1), 2))
+
+    new_genotype = tuple(chain(islice(genotype, break1),
+                               reversed(genotype[break1:break2]),
+                               islice(genotype, break2, num_genes)))
+
+    return new_genotype
