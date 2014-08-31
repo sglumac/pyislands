1df8fd0f64b221824fedc0c4b028dd770e61ece5
sglumac <slaven.glumac@gmail.com>
2014-08-31 16:44:07 +0200

    2opt mutation working

--- pyislands/permutation/tsp/mutation.py
+++ pyislands/permutation/tsp/mutation.py
@@ -9,6 +9,17 @@ def get_twoopt_mutation(adjacency_matrix, mutation_probability):
 
 
 def __twoopt_mutation(adjacency_matrix, mutation_probility, genotype):
+    '''
+    After each valid exchange_cost(i, j), tsp_cycle:
+
+    city_0, city_1, city_2, ..., city_i-1, city_i, ... city_j-1, city_j, ...,
+    city_n-1, city_0
+
+    becomes
+
+    city_0, city_1, city_2, ..., city_i-1, city_j, city_j-1, ..., city_i,
+    city_j+1..., city_n-1, city_0
+    '''
 
     if not random.random() < mutation_probility:
         return genotype
@@ -16,25 +27,25 @@ def __twoopt_mutation(adjacency_matrix, mutation_probility, genotype):
     tsp_cycle = list(chain([0], genotype, [0]))
     n = len(tsp_cycle)
 
-    distance = lambda a, b: adjacency_matrix[tsp_cycle[a]][tsp_cycle[b]]
+    distance = lambda idx1, idx2: adjacency_matrix[tsp_cycle[idx1]][tsp_cycle[idx2]]
 
-    def cost_exchange(i, j):
+    def exchange_cost(i, j):
 
         removed_cost = distance(i - 1, i) + distance(j - 1, j)
-        added_cost = distance(i - 1, j) + distance(j - 1, i)
+        added_cost = distance(i - 1, j - 1) + distance(i, j)
 
-        return removed_cost - added_cost
+        return added_cost - removed_cost
 
     exchange = True
-    idxs = list(range(1, n - 1))
+    idxs = range(1, n - 1)
     while exchange:
         exchange = False
         for i, j in combinations(idxs, 2):
-            if cost_exchange(i, j) < 0:
-                tsp_cycle = list(chain(islice(tsp_cycle, 0, i),
-                                       reversed(tsp_cycle[i:j + 1]),
-                                       islice(tsp_cycle, j + 1, n)))
+            if j > i + 1 and exchange_cost(i, j) < 0:
 
+                tsp_cycle = list(chain(islice(tsp_cycle, 0, i),
+                                       reversed(tsp_cycle[i:j]),
+                                       islice(tsp_cycle, j, n)))
                 exchange = True
 
     new_genotype = tuple(islice(tsp_cycle, 1, n - 1))
